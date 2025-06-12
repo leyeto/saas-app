67ac080b03e8522ed924fc9e5fe36a45cb3d1459
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2025-06-12 12:45:13 +0100

    created basic components for CompanaionCards

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,30 @@ const Page = () => {
       <h1 className="text-2xl underline">Popular Companions</h1>
 
       <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="124"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integers"
+          subject="maths"
+          duration={30}
+          color="#bde7ff"
+        />
+        <CompanionCard
+          id="125"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#d4d4d4"
+        />
       </section>
 
       <section className="home-section">
